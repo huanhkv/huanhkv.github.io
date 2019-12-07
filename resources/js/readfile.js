bb1ffc9912cb9909fec38932afc4c95c9e0ccae7
root <huanhkv@gmail.com>
2019-12-07 23:42:29 +0700

    Add json and use json

--- resources/js/readfile.js
+++ resources/js/readfile.js
@@ -1,12 +1,55 @@
-data = '[{"blue" : "is ok", "red" : "is my fave color"}]';
-path_json = 'post/content.json'
+var page = location.href.split("/").slice(-1)
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var myObj = JSON.parse(this.responseText);
-    console.log(myObj.post[0].title)
-  }
-};
-xmlhttp.open("GET", path_json, true);
-xmlhttp.send();
+var categories_url = 'data/categories.json';
+var posts_url = 'data/posts.json';
+var bucketList_url = 'data/bucket-list.json';
+
+function loadDoc(url, callback) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            callback(obj);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function readCategories(obj) {
+    // Create tag div for categories
+}
+
+function readPost(obj) {
+	$.each(obj.post, function(index, value){
+        value.categories
+		// var temp = "<li>"+ checked[value.checked] + " " + value.content + "</li>"
+        // $(".show-bucket-list").append(temp)
+        
+        // <a href="" class="card text-center">
+        //     <img src="images/Untitled.png" class="card-img-top" alt="...">
+        //     <div class="card-header">
+        //         <h5 class="card-title">Card title</h5>
+        //     </div>
+        //     <div class="card-body">
+        //         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        //     </div>
+        // </a>
+	})	
+}
+
+function readBucketList(obj) {
+	checked = ['✗', '✓'];
+	$.each(obj.bucket, function(index, value){
+		var temp = "<li>"+ checked[value.checked] + " " + value.content + "</li>"
+		$(".show-bucket-list").append(temp)
+	})	
+}
+
+if (page == "") {
+    loadDoc(categories_url, readCategories);
+    loadDoc(posts_url, readPost);
+} else if (page == "bucket.html") {
+    loadDoc(bucketList_url, readBucketList);
+}
